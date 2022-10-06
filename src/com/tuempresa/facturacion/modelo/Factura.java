3783abb32a406122e36d7915739c09a9acda8d90
AngelCruzPat <94579561+AngelCruzPat@users.noreply.github.com>
2022-10-06 12:17:56 -0500

    Leccion 4

--- src/com/tuempresa/facturacion/modelo/Factura.java
+++ src/com/tuempresa/facturacion/modelo/Factura.java
@@ -1,6 +1,7 @@
 package com.tuempresa.facturacion.modelo;
  
 import java.time.*;
+import java.util.*;
 
 import javax.persistence.*;
 import javax.persistence.Entity;
@@ -9,9 +10,17 @@ import org.hibernate.annotations.*;
 import org.openxava.annotations.*;
 import org.openxava.calculators.*;
 
+import com.tuempresa.facturacion.calculadores.*;
+
 import lombok.*;
  
 @Entity @Getter @Setter
+@View(members= 
+"anyo, numero, fecha;" +
+"cliente;" +
+"detalles;" +
+"observaciones"
+)
 public class Factura {
 
     @Id
@@ -26,13 +35,23 @@ public class Factura {
     int anyo;
  
     @Column(length=6)
+    @DefaultValueCalculator(value=CalculadorSiguienteNumeroParaAnyo.class,
+        properties=@PropertyValue(name="anyo")
+    )
     int numero;
  
     @Required
     @DefaultValueCalculator(CurrentLocalDateCalculator.class)
     LocalDate fecha;
- 
+    
+    @ManyToOne(fetch=FetchType.LAZY, optional=false)
+    @ReferenceView("Simple")
+    Cliente cliente;
+    
+    @ElementCollection
+    @ListProperties("producto.numero, producto.descripcion, cantidad")
+    Collection<Detalle> detalles;
+    
     @TextArea
-    String observaciones;
- 
+    String observaciones; 
 }
