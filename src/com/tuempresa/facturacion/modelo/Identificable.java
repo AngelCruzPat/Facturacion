3af6fe0eaebe6d122d5f6a37364575cc9ee585c7
AngelCruzPat <94579561+AngelCruzPat@users.noreply.github.com>
2022-10-10 22:17:39 -0500

    Leccion 6

--- src/com/tuempresa/facturacion/modelo/Identificable.java
+++ src/com/tuempresa/facturacion/modelo/Identificable.java
@@ -1,24 +1,19 @@
 package com.tuempresa.facturacion.modelo;
  
 import javax.persistence.*;
-import javax.persistence.Entity;
 
 import org.hibernate.annotations.*;
 import org.openxava.annotations.*;
 
 import lombok.*;
  
-@Entity @Getter @Setter
-public class Categoria extends Identificable  {
+@MappedSuperclass
+@Getter @Setter
+public class Identificable {
  
-    @Id
-    @Hidden
-    @GeneratedValue(generator="system-uuid")
+    @Id @GeneratedValue(generator="system-uuid") @Hidden
     @GenericGenerator(name="system-uuid", strategy = "uuid")
     @Column(length=32)
     String oid;
- 
-    @Column(length=50)
-    String descripcion;
  
 }
