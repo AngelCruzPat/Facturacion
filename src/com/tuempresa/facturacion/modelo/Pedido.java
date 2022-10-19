8dcb265873a61934937f27386547c36d06cadc42
Angel Cruz <angel.cruzpat@cbtis72.edu.mx>
2022-10-18 20:47:49 -0500

    Leccion 16
    Commit de la Leccion 16

--- src/com/tuempresa/facturacion/modelo/Pedido.java
+++ src/com/tuempresa/facturacion/modelo/Pedido.java
@@ -1,5 +1,7 @@
 package com.tuempresa.facturacion.modelo;
 
+import java.time.*;
+
 import javax.persistence.*;
 
 import org.openxava.annotations.*;
@@ -7,8 +9,10 @@ import org.openxava.annotations.*;
 import lombok.*;
 
 @Entity @Getter @Setter 
-@View(extendsView="super.DEFAULT",
-members="factura { factura } "
+@View(extendsView="super.DEFAULT", 
+	members=
+    "diasEntregaEstimados," + 
+	"factura { factura }"
 )
 @View( name="SinClienteNiFactura",
 members=                       
@@ -22,4 +26,20 @@ public class Pedido extends DocumentoComercial {
     @ReferenceView("SinClienteNiPedidos")
     private Factura factura;
 	
+	@Depends("fecha")
+	public int getDiasEntregaEstimados() {
+	    if (getFecha().getDayOfYear() < 15) {
+	        return 20 - getFecha().getDayOfYear(); 
+	    }
+	    if (getFecha().getDayOfWeek() == DayOfWeek.SUNDAY) return 2;
+	    if (getFecha().getDayOfWeek() == DayOfWeek.SATURDAY) return 3;
+	    return 1;
+	}
+	@Column(columnDefinition="INTEGER DEFAULT 1")
+	int diasEntrega;
+	
+	@PrePersist @PreUpdate 
+	private void recalcularDiasEntrega() {
+	    setDiasEntrega(getDiasEntregaEstimados());
+	}	
 }
